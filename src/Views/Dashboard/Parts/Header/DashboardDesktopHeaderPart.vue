63b02f576432152336ef119e4a3c409cdd054b6b
yuwahyu01 <nurhadi.wahyuu@gmail.com>
2025-08-01 21:01:49 +0700

    change name

--- src/Views/Dashboard/Parts/Header/DashboardDesktopHeaderPart.vue
+++ src/Views/Dashboard/Parts/Header/DashboardDesktopHeaderPart.vue
@@ -13,7 +13,7 @@
       <div class="relative">
         <button class="flex items-center space-x-2 text-gray-600 hover:text-gray-900 p-2 rounded-md">
           <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="User avatar">
-          <span class="text-sm font-medium">John Doe</span>
+          <span class="text-sm font-medium">Wahyu Nurhadi</span>
         </button>
       </div>
     </div>
